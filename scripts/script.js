8fd01c23c7288e5a72f3bad454ab6ae84cc5a41b
Irina Demina <ir-mars@yandex.ru>
2022-11-15 01:04:36 +0600

    попап

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const popupElement = document.querySelector('.popup');
+console.log(popupElement);
+
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
+console.log(popupCloseButtonElement);
+
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+console.log(popupOpenButtonElement);
