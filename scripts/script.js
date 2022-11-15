32a9ce34e4b760a906633ca6488a9e347f08910c
Irina Demina <ir-mars@yandex.ru>
2022-11-15 15:31:21 +0600

    готовый проект

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,36 @@
 const popupElement = document.querySelector('.popup');
-console.log(popupElement);
-
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-btn');
-console.log(popupCloseButtonElement);
-
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-console.log(popupOpenButtonElement);
+const popupInputName = popupElement.querySelector('.popup__input_type_name');
+const popupInputDescription = popupElement.querySelector('.popup__input_type_description');
+const profileElement = document.querySelector('.profile');
+const profileTitleElement = profileElement.querySelector('.profile__title');
+const profileSubtitleElement = profileElement.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup__form');
+
+
+const openPopup = function () {
+    popupElement.classList.add('popup_is-opened');
+    popupInputName.value = profileTitleElement.textContent;
+    popupInputDescription.value = profileSubtitleElement.textContent;
+};
+
+const closePopup = function () {
+    popupElement.classList.remove('popup_is-opened');
+};
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitleElement.textContent = popupInputName.value;
+    profileSubtitleElement.textContent = popupInputDescription.value;
+};
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
